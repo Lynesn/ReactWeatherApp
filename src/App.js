cd9f2ac81dc573773804e785a9458d74d7a0c514
=lyne <=lynealex@gmail.com>
2022-10-02 00:56:44 +0100

    worked on the html files.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
+import Weather from "/Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+
+        <Weather/>
         <footer>
       This project was made by Lyne N and is {" "}
           <a href="https://github.com/Lynesn/ReactWeatherApp" target="blank">open-sourced on Github</a>
---
+++ src/Weather.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Weather.css";
+
+export default function App() {
+  return (
+    <div className="Weather">
+        <form>
+            <div className="row">
+                <div className="col-9">
+                    <input type="search" placeholder="Enter a city" className="form-control"/>
+                    <input type="submit" value="Search" className="btn btn-primary"/>
+                </div>
+            </div>
+        </form>
+        <h1>Kampala</h1>
+        <ul>
+            <li>Wed 07:00</li>
+            <li>Mostly Cloudy</li>
+        </ul>
+        <div className="row">
+            <div className="col-6">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy"/>
+                6°C
+            </div>
+            <div className="col-6">
+                <ul>
+                    <li>Precipitation: 2%</li>
+                    <li>Humidity: 84%</li>
+                    <li>Wind: 9 km/h</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  );
+}
+
