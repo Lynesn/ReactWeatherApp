09c500af397c92c5448c78a0f0b4ab012a876f90
=lyne <=lynealex@gmail.com>
2022-10-01 23:49:27 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-    <footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
       This project was made by Lyne N and is {" "}
-      <a href="https://github.com/Lynesn/ReactWeatherApp" target="blank">open-sourced on Github</a>
-    </footer>
+          <a href="https://github.com/Lynesn/ReactWeatherApp" target="blank">open-sourced on Github</a>
+        </footer>
+      </div>
     </div>
   );
 }
